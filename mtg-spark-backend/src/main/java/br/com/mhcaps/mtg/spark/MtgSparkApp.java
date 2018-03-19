79fb691232f8e718d647c140070e154c9b2850ba
Mauro Henrique Campanha <mhc.sor@gmail.com>
2018-03-18 21:19:04 -0300

    Back-end project configuration

---
+++ mtg-spark-backend/src/main/java/br/com/mhcaps/mtg/spark/MtgSparkApp.java
@@ -0,0 +1,12 @@
+package br.com.mhcaps.mtg.spark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgSparkApp {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MtgSparkApp.class, args);
+    }
+}
---
+++ mtg-spark-backend/src/main/java/br/com/mhcaps/mtg/spark/configuration/WebMvcConfiguration.java
@@ -0,0 +1,36 @@
+package br.com.mhcaps.mtg.spark.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = { "classpath:/public/" };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        super.addResourceHandlers(registry);
+
+        if (!registry.hasMappingForPattern("/webjars/**")) {
+            registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        }
+        if (!registry.hasMappingForPattern("/**")) {
+            registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+        }
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**").allowedOrigins("http://localhost:8080");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+    }
+}
